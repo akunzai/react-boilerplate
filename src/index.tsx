0fc5030ebfa4e96d99c48ea859b684d9014a1012
Charley Wu <akunzai@gmail.com>
2021-09-14 22:28:46 +0800

    Renamed App.scss to index.scss

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './i18n';
+import './index.scss';
 import reportWebVitals from './reportWebVitals';
 
 // Start the mocking conditionally.
