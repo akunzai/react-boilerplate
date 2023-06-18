fecc03848c9f08cb918c67aa3b6d49d4175320a4
Charley Wu <akunzai@gmail.com>
2023-06-18 11:57:56 +0800

    Use Router base

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,33 @@
-import { Redirect, Route, Switch } from 'wouter';
+import { Redirect, Route, Router, Switch } from 'wouter';
 import { Counter, Home, Layout, TodoDetail, TodoList } from './components';
 
 export default function App(): JSX.Element {
+  const baseUrl = document
+    .getElementsByTagName('base')[0]
+    ?.getAttribute('href')
+    ?.replace(/[/]$/, '');
   // here app catches the suspense from page in case translations are not yet loaded
   return (
-    <Layout>
-      <Switch>
-        <Route path='/'>
-          <Home />
-        </Route>
-        <Route path='/counter'>
-          <Counter />
-        </Route>
-        <Route path='/todo-list'>
-          <TodoList />
-        </Route>
-        <Route path='/todo/:id'>
-          {(params) => <TodoDetail id={Number(params.id)} />}
-        </Route>
-        <Route>
-          <Redirect to='/' />
-        </Route>
-      </Switch>
-    </Layout>
+    <Router base={baseUrl || ''}>
+      <Layout>
+        <Switch>
+          <Route path='/'>
+            <Home />
+          </Route>
+          <Route path='/counter'>
+            <Counter />
+          </Route>
+          <Route path='/todo-list'>
+            <TodoList />
+          </Route>
+          <Route path='/todo/:id'>
+            {(params) => <TodoDetail id={Number(params.id)} />}
+          </Route>
+          <Route>
+            <Redirect to='/' />
+          </Route>
+        </Switch>
+      </Layout>
+    </Router>
   );
 }
