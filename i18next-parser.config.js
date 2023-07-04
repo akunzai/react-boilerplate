1fe49d143e04b0c24eed82f354e9c604131d8567
Charley Wu <akunzai@gmail.com>
2023-07-04 11:38:57 +0800

    Convert i18next-parser config to ES module

--- i18next-parser.config.js
+++ i18next-parser.config.js
@@ -1,4 +1,5 @@
-module.exports = {
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
   input: ['src/**/*.tsx'],
   output: 'src/locales/$LOCALE/$NAMESPACE.json',
   locales: ['zh-Hant'],
