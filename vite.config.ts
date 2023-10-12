d93f35160130455cc2c6c2895d36bd433bec3bd5
Charley Wu <akunzai@gmail.com>
2023-10-12 22:10:38 +0800

    Update NPM packages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    target: 'esnext'
+  },
   plugins: [react()],
   server: {
     open: true,
