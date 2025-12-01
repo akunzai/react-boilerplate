85d8e80591c6c396ecc8724d194e819010d3e55b
Charley Wu <akunzai@gmail.com>
2025-12-01 13:02:50 +0800

    Add localStorage/sessionStorage polyfill for Node.js 22+

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: './src/setupTests.ts',
+    setupFiles: ['./src/setupStoragePolyfill.ts', './src/setupTests.ts'],
     coverage: {
       reporter: ['text', 'clover'],
       include: ['src/**/*.{ts,tsx}'],
